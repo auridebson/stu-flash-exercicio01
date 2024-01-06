d4644f53903aeb08dbd4707a1e85b5b8d10b5a8a
Auridebson Lima <auridebson@vutus.com>
2024-01-06 16:23:09 -0300

    Update

---
+++ script.js
@@ -0,0 +1,41 @@
+const btnAciona = document.querySelector(".btnAciona")
+const containerName = document.querySelector(".container-name")
+
+const alertaEF = () => alert("Mostrando mensagem\nCom Expressão de função - Arrow")
+const dt = new Date()
+const infoData = () => alert(`A data coletada nessa Arrow Function é\n${dt.getDay()}/${dt.getMonth()}/${dt.getFullYear()}`)
+// const nomeUser = prompt("Digite seu mome")
+
+const infoNome = () => {
+    return containerName.innerHTML += nomeUser + '<br>'
+}
+const person = {
+    name: "Auridebson", 
+    age: 46,
+    city: "Fortaleza"
+}
+
+const mostraNome = () => containerName.innerHTML = `${person.name} -  ${person.age} - ${person.city}`
+
+function alertaDF() {
+    alert("Mostrando mensagem\nCom Declaração de função")
+}
+
+
+const findMaxNumber = numeros => Math.max(...numeros)
+
+const checkPair = numero => (numero % 2 == 0 ? "Par" : "Impar")
+
+const checkImpar = numero => (numero % 2 == 0 ? "Par":"Impar")
+
+const countText = (textoo) => textoo.replace(/\s/g,"").length;
+
+const num = numero => (numero > 0 ? "Positivo" : "Neutro")
+
+const numRandom = numero => Math.random(numero)
+
+<<<<<<< HEAD
+btnAciona.addEventListener("click", num)
+=======
+btnAciona.addEventListener("click", alertaDF)
+>>>>>>> cdb6e3c662178a873d70efd81728d855fa325b57
